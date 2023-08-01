6c474ae3f867843f0253be5f30116806e4cbad75
Leonardo Moura <leonardomourasagaa@gmail.com>
2023-08-01 15:56:43 -0300

    feat: create-page-cookies-consent

---
+++ src/pages/cookies-accept-message/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { initializeFirebaseApp } from "../../config/firebase";
+import { COOKIES_CONSENT_STORAGE_KEY } from "../../constants";
+import { getStorage, setStorage } from "../../utils/storage";
+import { Button, Snackbar, SnackbarOrigin, Stack } from "@mui/material";
+import { ROUTING_PATH } from "../../routes/routes";
+
+const CookiesAcceptMessage: React.FC = () => {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
+  useEffect(() => {
+    initializeFirebaseApp();
+    const consent = getStorage(COOKIES_CONSENT_STORAGE_KEY);
+    if (!consent) {
+      setShowSnackbar(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    setStorage(COOKIES_CONSENT_STORAGE_KEY, true.toString());
+    setShowSnackbar(false);
+  };
+
+  const handleRefused = () => {
+    setStorage(COOKIES_CONSENT_STORAGE_KEY, false.toString());
+    setShowSnackbar(false);
+  };
+
+  const snackbarOrigin: SnackbarOrigin = {
+    vertical: "bottom",
+    horizontal: "center",
+  };
+
+  return (
+    <Snackbar
+      open={showSnackbar}
+      message={
+        <>
+          Nós usamos cookies e outras tecnologias semelhantes para melhorar a
+          sua experiência em nossos serviços. Ao utilizar nossos serviços, você
+          concorda com nossas condições. Para saber mais acesse nossa{" "}
+          <a href={ROUTING_PATH.PRIVACY_POLICY}>
+            Política de Privacidade e Uso de Cookies
+          </a>
+        </>
+      }
+      anchorOrigin={snackbarOrigin}
+      action={
+        <Stack direction="row" spacing={1}>
+          <Button variant="contained" onClick={handleAccept}>
+            Aceitar
+          </Button>
+          <Button variant="contained" onClick={handleRefused}>
+            Recusar
+          </Button>
+        </Stack>
+      }
+    />
+  );
+};
+
+export default CookiesAcceptMessage;
