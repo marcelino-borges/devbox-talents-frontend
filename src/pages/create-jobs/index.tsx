480c589da65d141af37df793b28019f893046283
Leonardo Moura <leonardomourasagaa@gmail.com>
2023-08-07 16:12:51 -0300

    feat: create page register jobs

--- src/pages/create-jobs/index.tsx
+++ src/pages/create-jobs/index.tsx
@@ -1,15 +1,30 @@
 import {
+  Autocomplete,
   Box,
+  Button,
   MenuItem,
   Select,
   SelectChangeEvent,
   Stack,
   TextField,
 } from "@mui/material";
+import { Clear as ClearIcon } from "@mui/icons-material";
 import React, { useState } from "react";
-import { EmploymentType, LocationType } from "../../models/talents";
-import { translateEmploymentType, translateLocationType } from "../../utils";
+import { EmploymentType, LocationType, Skill } from "../../models/talents";
+import {
+  translateEmploymentType,
+  translateLocationType,
+  translateSeniorityLevel,
+} from "../../utils";
 import { Job, SeniorityLevel } from "../../models/jobs";
+import {
+  DATABASES,
+  FRAMEWORKS,
+  LANGUAGES,
+  MAX_APP_WIDTH,
+  OTHER_SKILLS,
+} from "../../constants";
+import Footer from "../../components/footer";
 
 const EMPTY_PROFESSION: Job = {
   creatorId: undefined,
@@ -28,6 +43,59 @@ const EMPTY_PROFESSION: Job = {
 
 const CreateJobs: React.FC = () => {
   const [jobToAdd, setJobToAdd] = useState<Job>(EMPTY_PROFESSION);
+  const [languageToAdd, setLanguageToAdd] = useState<Skill>();
+  const [addedLanguages, setAddedLanguages] = useState<Skill[]>([]);
+  const [frameworkToAdd, setFrameworkToAdd] = useState<Skill>();
+  const [addedFrameworks, setAddedFrameworks] = useState<Skill[]>([]);
+  const [addedDatabases, setAddedDatabases] = useState<Skill[]>([]);
+  const [databaseToAdd, setDatabaseToAdd] = useState<Skill>();
+  const [addedOtherSkills, setAddedOtherSkills] = useState<Skill[]>([]);
+  const [otherSkillsToAdd, setOtherSkillsToAdd] = useState<Skill>();
+
+  const SelectedSkills = ({ skills, setSkills }: any) => {
+    if (!skills.length) return null;
+
+    return (
+      <Box
+        style={{
+          border: "1px dashed #e4e4e4",
+          borderRadius: "10px",
+        }}
+      >
+        <Stack direction="row" gap="16px" p="8px" flexWrap="wrap">
+          {skills.map((skill: Skill) => (
+            <Stack
+              key={skill.label}
+              direction="row"
+              gap="4px"
+              style={{
+                borderRadius: "5px",
+                backgroundColor: "#00000008",
+                padding: "4px",
+                fontSize: "0.7rem",
+              }}
+              alignItems="center"
+            >
+              {skill.label}
+              <ClearIcon
+                style={{
+                  color: "#00000040",
+                  fontSize: "1.2rem",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  const updatedList = skills.filter(
+                    (addedSkill: Skill) => skill.value !== addedSkill.value
+                  );
+                  setSkills(updatedList);
+                }}
+              />
+            </Stack>
+          ))}
+        </Stack>
+      </Box>
+    );
+  };
 
   const SectionTitle = ({ text }: { text: string }) => {
     return (
@@ -146,6 +214,31 @@ const CreateJobs: React.FC = () => {
               })}
           </Select>
         </Stack>
+        <Select
+          variant="outlined"
+          fullWidth
+          required
+          value={jobToAdd.seniorityLevel.toString()}
+          onChange={(event: SelectChangeEvent) => {
+            if (!jobToAdd) return;
+
+            const levelSenior = {
+              ...jobToAdd,
+              seniorityLevel: Number(event.target.value),
+            };
+            setJobToAdd(levelSenior);
+          }}
+        >
+          {Object.keys(SeniorityLevel)
+            .filter((key: any) => isNaN(key))
+            .map((type: string) => {
+              return (
+                <MenuItem value={SeniorityLevel[type as any]} key={type}>
+                  {translateSeniorityLevel(SeniorityLevel[type as any] as any)}
+                </MenuItem>
+              );
+            })}
+        </Select>
         <TextField
           label="Localização"
           variant="outlined"
@@ -181,7 +274,398 @@ const CreateJobs: React.FC = () => {
             setJobToAdd(description);
           }}
         />
+        <TextField
+          label="Requisitos Obrigatorios"
+          variant="outlined"
+          fullWidth
+          multiline
+          required
+          minRows={4}
+          value={jobToAdd.mandatoryRequirements}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            if (!jobToAdd) return;
+
+            const requirements = {
+              ...jobToAdd,
+              mandatoryRequirements: event.target.value,
+            };
+            setJobToAdd(requirements);
+          }}
+        />
+        <TextField
+          label="Requisitos Preferidos"
+          variant="outlined"
+          fullWidth
+          multiline
+          minRows={4}
+          value={jobToAdd.preferredRequirements}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            if (!jobToAdd) return;
+
+            const requirementsPreferred = {
+              ...jobToAdd,
+              preferredRequirements: event.target.value,
+            };
+            setJobToAdd(requirementsPreferred);
+          }}
+        />
+        <br />
+        <SectionTitle text="Habilidades Obrigatória" />
+        <br />
+        <Stack direction="row" gap="16px">
+          <Box flexGrow={1}>
+            <Autocomplete
+              selectOnFocus
+              clearOnBlur
+              handleHomeEndKeys
+              renderInput={(params) => (
+                <TextField {...params} label="Linguagens" />
+              )}
+              options={LANGUAGES}
+              value={languageToAdd}
+              onChange={(_: unknown, value: Skill | null) =>
+                setLanguageToAdd(value ?? undefined)
+              }
+            />
+          </Box>
+          <Box flexGrow={0}>
+            <Button
+              variant="contained"
+              style={{ height: "100%" }}
+              onClick={() => {
+                if (
+                  languageToAdd &&
+                  !addedLanguages.some(
+                    (skill: Skill) => skill.value === languageToAdd.value
+                  )
+                ) {
+                  setAddedLanguages([...(addedLanguages ?? []), languageToAdd]);
+                }
+              }}
+            >
+              Add
+            </Button>
+          </Box>
+        </Stack>
+        <SelectedSkills skills={addedLanguages} setSkills={setAddedLanguages} />
+        <Stack direction="column" gap="16px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Frameworks" />
+                )}
+                options={FRAMEWORKS}
+                value={frameworkToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setFrameworkToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    frameworkToAdd &&
+                    !addedFrameworks.some(
+                      (skill: Skill) => skill.value === frameworkToAdd.value
+                    )
+                  ) {
+                    setAddedFrameworks([
+                      ...(addedFrameworks ?? []),
+                      frameworkToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedFrameworks}
+            setSkills={setAddedFrameworks}
+          />
+        </Stack>
+        <Stack direction="column" gap="16px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Bancos de dados" />
+                )}
+                options={DATABASES}
+                value={frameworkToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setDatabaseToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    databaseToAdd &&
+                    !addedDatabases.some(
+                      (skill: Skill) => skill.value === databaseToAdd.value
+                    )
+                  ) {
+                    setAddedDatabases([
+                      ...(addedDatabases ?? []),
+                      databaseToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedDatabases}
+            setSkills={setAddedDatabases}
+          />
+        </Stack>
+        <Stack direction="column" gap="16px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Outras habilidades" />
+                )}
+                options={OTHER_SKILLS}
+                value={otherSkillsToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setOtherSkillsToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    otherSkillsToAdd &&
+                    !addedOtherSkills.some(
+                      (skill: Skill) => skill.value === otherSkillsToAdd.value
+                    )
+                  ) {
+                    setAddedOtherSkills([
+                      ...(addedOtherSkills ?? []),
+                      otherSkillsToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedOtherSkills}
+            setSkills={setAddedOtherSkills}
+          />
+        </Stack>
+        <br />
+        <SectionTitle text="Conhecimentos Diferenciais" />
+        <br />
+        <Stack direction="row" gap="16px">
+          <Box flexGrow={1}>
+            <Autocomplete
+              selectOnFocus
+              clearOnBlur
+              handleHomeEndKeys
+              renderInput={(params) => (
+                <TextField {...params} label="Linguagens" />
+              )}
+              options={LANGUAGES}
+              value={languageToAdd}
+              onChange={(_: unknown, value: Skill | null) =>
+                setLanguageToAdd(value ?? undefined)
+              }
+            />
+          </Box>
+          <Box flexGrow={0}>
+            <Button
+              variant="contained"
+              style={{ height: "100%" }}
+              onClick={() => {
+                if (
+                  languageToAdd &&
+                  !addedLanguages.some(
+                    (skill: Skill) => skill.value === languageToAdd.value
+                  )
+                ) {
+                  setAddedLanguages([...(addedLanguages ?? []), languageToAdd]);
+                }
+              }}
+            >
+              Add
+            </Button>
+          </Box>
+          <SelectedSkills
+            skills={addedLanguages}
+            setSkills={setAddedLanguages}
+          />
+        </Stack>
+        <Stack direction="column" gap="16px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Frameworks" />
+                )}
+                options={FRAMEWORKS}
+                value={frameworkToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setFrameworkToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    frameworkToAdd &&
+                    !addedFrameworks.some(
+                      (skill: Skill) => skill.value === frameworkToAdd.value
+                    )
+                  ) {
+                    setAddedFrameworks([
+                      ...(addedFrameworks ?? []),
+                      frameworkToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedFrameworks}
+            setSkills={setAddedFrameworks}
+          />
+        </Stack>
+        <Stack direction="column" gap="16px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Bancos de dados" />
+                )}
+                options={DATABASES}
+                value={frameworkToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setDatabaseToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    databaseToAdd &&
+                    !addedDatabases.some(
+                      (skill: Skill) => skill.value === databaseToAdd.value
+                    )
+                  ) {
+                    setAddedDatabases([
+                      ...(addedDatabases ?? []),
+                      databaseToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedDatabases}
+            setSkills={setAddedDatabases}
+          />
+        </Stack>
+        <Stack direction="column" gap="16px" mb="30px">
+          <Stack direction="row" gap="16px">
+            <Box flexGrow={1}>
+              <Autocomplete
+                selectOnFocus
+                clearOnBlur
+                handleHomeEndKeys
+                renderInput={(params) => (
+                  <TextField {...params} label="Outras habilidades" />
+                )}
+                options={OTHER_SKILLS}
+                value={otherSkillsToAdd}
+                onChange={(_: unknown, value: Skill | null) =>
+                  setOtherSkillsToAdd(value ?? undefined)
+                }
+              />
+            </Box>
+            <Box flexGrow={0}>
+              <Button
+                variant="contained"
+                style={{ height: "100%" }}
+                onClick={() => {
+                  if (
+                    otherSkillsToAdd &&
+                    !addedOtherSkills.some(
+                      (skill: Skill) => skill.value === otherSkillsToAdd.value
+                    )
+                  ) {
+                    setAddedOtherSkills([
+                      ...(addedOtherSkills ?? []),
+                      otherSkillsToAdd,
+                    ]);
+                  }
+                }}
+              >
+                Add
+              </Button>
+            </Box>
+          </Stack>
+          <SelectedSkills
+            skills={addedOtherSkills}
+            setSkills={setAddedOtherSkills}
+          />
+        </Stack>
+        <Button fullWidth variant="contained">
+          Criar Vaga
+        </Button>
       </Stack>
+      <Box
+        width="100%"
+        maxWidth={MAX_APP_WIDTH}
+        textAlign="left"
+        fontSize="0.8em"
+      >
+        <Footer />
+      </Box>
     </Stack>
   );
 };
